ea36c4a92c83b3ce5da48bd116e771e352993595
Patrik Huzina <patrikhuzina@gmail.com>
2024-09-28 20:40:59 +0200

    tailwind fix

--- frontend/src/components/NotesMain.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-import "../index.css";
-
-export const NotesMain = () => {
-  const [notes, setNotes] = useState([]);
-  return <div className={"w-screen h-screen bg-black"}>oi</div>;
-};
