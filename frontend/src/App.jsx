c79a5761461416389309b2f795cdaf56b76d83ea
Tóth Áron Pál <ronyka1009@gmail.com>
2024-09-28 20:29:07 +0200

    tailwind error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
+import { NotesMain } from "./components/NotesMain.jsx";
+
 function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Hello, Vite + Tailwind!
-      </h1>
-    </div>
-  );
+  return <NotesMain />;
 }
 
 export default App;
---
+++ frontend/src/components/NotesMain.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import "../index.css";
+
+export const NotesMain = () => {
+  const [notes, setNotes] = useState([]);
+  return <div className={"w-screen h-screen bg-black"}>oi</div>;
+};
