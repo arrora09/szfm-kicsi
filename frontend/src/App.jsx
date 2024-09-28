9957fae2b49b68efd528c2b117ca94faab0ed6ea
Patrik Huzina <patrikhuzina@gmail.com>
2024-09-28 20:14:02 +0200

    Initial commit, architecture created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Vite + Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
