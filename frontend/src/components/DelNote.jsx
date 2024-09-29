8f5e3f509156022319f3f72bbe5418d4f52dbc2e
Margitai Balázs <balazsmargitai@gmail.com>
2024-09-29 21:29:58 +0200

    frontend: delete list, restore. permanent-delete

--- frontend/src/components/DelNote.jsx
+++ frontend/src/components/DelNote.jsx
@@ -8,8 +8,47 @@ export const DelNote = (props) => {
   useEffect(() => {
     setDeleted(props.deleted);
   }, [props.deleted]);
+
+  const delNote = async () => {
+      props.setDeleted([]);
+      const response = await fetch("http://localhost:4000/save", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([...props.notes]),
+      });
+      if (!response.ok) {
+        console.error("Error saving data after restoring note");
+      }
+  }
+
+  const restoreNote = async (noteId) => {
+    const noteToRestore = deleted.find((note) => note.id === noteId);
+    if (!noteToRestore) return;
+
+    const updatedNotes = [...props.notes, { ...noteToRestore, isInTrash: false}];
+    const updatedDeleted = deleted.filter((note) => note.id !== noteId);
+
+    props.setNotes(updatedNotes);
+    props.setDeleted(updatedDeleted);
+
+    const response = await fetch("http://localhost:4000/save", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([...updatedNotes, ...updatedDeleted]),
+    });
+
+    if (!response.ok) {
+      console.error("Error saving data after restoring note");
+    }
+  };
+
+
   return (
-    <div className={"w-fit min-h-fit p-3 bg-gray-200 text-black rounded-xl"}>
+    <div className={"w-96 min-h-fit p-3 bg-gray-200 text-black rounded-xl"}>
       <div
         className={
           "mb-4 w-full text-center flex flex-row justify-between items-center"
@@ -30,9 +69,19 @@ export const DelNote = (props) => {
       <div className={"mb-4"}>
         <h3 className="text-lg font-semibold mb-2">Törlendő jegyzetek:</h3>
         <ul className="list-disc pl-5 text-sm">
-          {props.notes.map((note, index) => (
-            <li key={index} className="mb-1">
-              {note.title}
+          {props.deleted.map((note, index) => (
+            <li key={index}
+                className="mb-1 flex flex-row justify-between items-center text-black"
+                >
+                  <p
+                  className="text-xl max-w-72 font-semibold">
+                    {note.title}
+                  </p>
+                  <button
+                  className="bg-green text-white hover:cursor-pointer hover:text-black transition-all w-fit h-fit"
+                  onClick={() => restoreNote(note.id)}>
+                    Visszaállítás
+                  </button>
             </li>
           ))}
         </ul>
@@ -40,12 +89,12 @@ export const DelNote = (props) => {
 
       <div className="flex justify-end mt-4">
         <button
-          onClick={() => props.delNote()}
-          className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-all"
+          onClick={() => delNote()}
+          className="px-4 py-2 bg-red text-white rounded-md hover:bg-red-hover hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-all"
         >
           Törlés
         </button>
       </div>
     </div>
   );
-};
+}
